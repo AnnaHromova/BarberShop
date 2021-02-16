d649f9b83ea38014a407debf0d859977bdc99385
AnnaHromova <77169668+AnnaHromova@users.noreply.github.com>
2021-02-16 13:29:03 +0200

    new

---
+++ app/js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+	const swiper = new Swiper('.swiper-container', {
+		loop: true,
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		autoplay: {
+			delay: 5000,
+		},
+	});
+
+	//Login modal
+	$('.js-login').on('click', function () {
+		$('.modal--login').fadeIn(400);
+	});
+
+	$('.modal__close').on('click', function () {
+		$('.modal--login').fadeOut(400);
+	});
+
+	$(document).on('mouseup', function (event) {
+		const LoginModal = $('.modal__window');
+		if (!LoginModal.is(event.target) && LoginModal.has(event.target).length === 0) {
+			$('.modal').fadeOut(400);
+		}
+
+
+	});
+})
